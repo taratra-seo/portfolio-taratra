bfe313b847b88ae7c5ac51afee99240b48ccf7eb
taratra-seo <taratraandriamanamihanta@gmail.com>
2025-11-11 18:42:00 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+// Welcome to your project!
+// Use this file to add interactivity to your pages.
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Project ready. Happy building!');
+});
